dc5207aad029be7b3c5b27c232eb837eaa6d7f02
Yee Hui Poh <yhpoh91@gmail.com>
2020-10-13 15:13:09 +0800

    add support for generating token

--- src/server.js
+++ src/server.js
@@ -6,8 +6,13 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const http = require('http');
 const helmet = require('helmet');
+const OpenTok = require('opentok');
 
 const port = process.env.PORT || 8080;
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.API_SECRET;
+
+const client = new OpenTok(apiKey, apiSecret);
 
 // Always use UTC Timezone
 process.env.TZ = 'Etc/UTC';
@@ -23,6 +28,20 @@ app.use(bodyParser.json({ limit: requestMaxSize }));
 
 app.use('/', express.static('public'));
 app.get('/success', (_, res) => res.send('You have successfully deployed the Simple Opentok Audio Level'));
+
+app.get('/token', async (req, res, next) => {
+  try {
+    const { sessionId } = req.query;
+    const token = client.generateToken(sessionId);
+    res.json({
+      apiKey,
+      sessionId,
+      token,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Create Application HTTP Server
 const httpServer = http.createServer(app);
