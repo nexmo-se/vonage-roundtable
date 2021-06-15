5875ef0db37bc0ac83fa45f908e80c625c234642
rktalusani <rajkiran.talusani@vonage.com>
2021-06-15 17:33:01 +0800

    added audio stats

--- public/lib/stats/stats.js
+++ public/lib/stats/stats.js
@@ -9,16 +9,27 @@ OTStats = (options) => {
   let subscribers = {};
   let onPublisherStatsAvailableListener = null;
   let onSubscriberStatsAvailableListener = null;
-  let statsTimer = null;
+  let statsTimer = undefined;
   
+  const removeSubscriber = (streamId) => {
+      delete subscribers[streamId];
+  }
   const addSubscriber = (subscriber) => {
-      subscribers[subscriber.id] = {
+      subscribers[subscriber.stream.id] = {
       id: subscriber.id,
       subscriber,
-      previousTimestamp:0,
-      prevPublisherBytesReceived:0,
-      prevPacketsLost:0,
-      prevPacketsReceived:0
+      video:{
+          previousTimestamp:0,
+          prevBytesReceived:0,
+          prevPacketsLost:0,
+          prevPacketsReceived:0
+      },
+      audio:{
+          previousTimestamp:0,
+          prevBytesReceived:0,
+          prevPacketsLost:0,
+          prevPacketsReceived:0
+      }
     };
   }
   const setPublisher = (pub) => {
@@ -27,7 +38,16 @@ OTStats = (options) => {
   }
   
   const start = () => {
-      statsTimer = setInterval(collectStats,config.statsInterval);
+      if(statsTimer === undefined){
+        statsTimer = setInterval(collectStats,config.statsInterval);
+      }
+  }
+  
+  const stop = () => {
+      if(statsTimer !== undefined){
+          clearInterval(statsTimer);
+          statsTimer = undefined;
+      }
   }
   const collectStats = () => {
       
@@ -47,8 +67,9 @@ OTStats = (options) => {
                 const packetsSent = videoStats.packetsSent - publisherHistory.prevPacketsSent;
                 let totalPkts = packetLost + packetsSent;
                 let PLRatio = totalPkts > 0 ? (packetLost/totalPkts) : 0;
+                PLRatio = Math.round(PLRatio*100)/100;
               
-                const videoKbps = Math.floor((videoStats.bytesSent-publisherHistory.prevPublisherBytesSent)*8/1000000*(statsArray[0].stats.timestamp-publisherHistory.previousTimestamp));
+                const videoKbps = Math.floor(((videoStats.bytesSent-publisherHistory.prevPublisherBytesSent)*8)/(statsArray[0].stats.timestamp-publisherHistory.previousTimestamp));
               
                 publisherHistory.prevPublisherBytesSent = videoStats.bytesSent;
                 publisherHistory.prevPacketsSent = videoStats.packetsSent;
@@ -56,7 +77,7 @@ OTStats = (options) => {
                 publisherHistory.previousTimestamp = statsArray[0].stats.timestamp;
                 
                 if(onPublisherStatsAvailableListener != null){
-                    onPublisherStatsAvailableListener(videoKbps,videoStats.frameRate,PLRatio);
+                    onPublisherStatsAvailableListener(videoKbps,publisher.videoWidth(),publisher.videoHeight(),Math.round(videoStats.frameRate),PLRatio);
                 }
                 else {
                     console.log("onStatsAvailableListener is null");
@@ -69,36 +90,65 @@ OTStats = (options) => {
       
       /* subscriber stats */
       for(var subscriberId in subscribers){
-          console.log(subscriberId);
+          //console.log(subscriberId);
           let sub = subscribers[subscriberId].subscriber;
-          console.log(sub);
+          //console.log(sub);
+          
           sub.getStats((err, statsArray) => {
                 if(err){
                     console.log(err);
                     return;
                 }
+                //console.log(statsArray);
                 
-                if(subscribers[subscriberId].previousTimestamp === 0){
-                    subscribers[subscriberId].previousTimestamp = statsArray[0].stats.timestamp;
+                if(subscribers[subscriberId].video.previousTimestamp === 0){
+                    subscribers[subscriberId].video.previousTimestamp = statsArray.timestamp;
+                    subscribers[subscriberId].video.previousTimestamp = statsArray.timestamp;
                     return;
                 }
-                console.log(statsArray);
-                const audioStats = statsArray[0].stats.audio;
-                const videoStats = statsArray[0].stats.video;
-                const packetLost = videoStats.packetsLost - subscribers[subscriberId].prevPacketsLost;
-                const packetsReceived = videoStats.packetsReceived - subscribers[subscriberId].prevPacketsReceived;
-                let totalPkts = packetLost + packetsReceived;
-                let PLRatio = totalPkts > 0 ? (packetLost/totalPkts) : 0;
+                
+                const audioStats = statsArray.audio;
+                const videoStats = statsArray.video;
               
-                const videoKbps = Math.floor((videoStats.bytesReceived-subscribers[subscriberId].prevPublisherBytesReceived)*8/1000000*(statsArray[0].stats.timestamp-subscribers[subscriberId].previousTimestamp));
+                const videoPacketLost = videoStats.packetsLost - subscribers[subscriberId].video.prevPacketsLost;
+                const videoPacketsReceived = videoStats.packetsReceived - subscribers[subscriberId].video.prevPacketsReceived;
+                let videoTotalPkts = videoPacketLost + videoPacketsReceived;
+                let videoPLRatio = videoTotalPkts > 0 ? (videoPacketLost/videoTotalPkts) : 0;
+                videoPLRatio = Math.round(videoPLRatio*100)/100;
+               
+                const audioPacketLost = audioStats.packetsLost - subscribers[subscriberId].audio.prevPacketsLost;
+                const audioPacketsReceived = audioStats.packetsReceived - subscribers[subscriberId].audio.prevPacketsReceived;
+                let audioTotalPkts = audioPacketLost + audioPacketsReceived;
+                let audioPLRatio = audioTotalPkts > 0 ? (audioPacketLost/audioTotalPkts) : 0;
+                audioPLRatio = Math.round(audioPLRatio*100)/100;
               
-                subscribers[subscriberId].prevPublisherBytesReceived = videoStats.bytesReceived;
-                subscribers[subscriberId].prevPacketsReceived = videoStats.packetsReceived;
-                subscribers[subscriberId].prevPacketsLost = videoStats.packetsLost;
-                subscribers[subscriberId].previousTimestamp = statsArray[0].stats.timestamp;
+                const videoKbps = Math.floor(((videoStats.bytesReceived-subscribers[subscriberId].video.prevBytesReceived)*8)/(statsArray.timestamp-subscribers[subscriberId].video.previousTimestamp));
+              
+                const audioKbps = Math.floor(((audioStats.bytesReceived-subscribers[subscriberId].audio.prevBytesReceived)*8)/(statsArray.timestamp-subscribers[subscriberId].audio.previousTimestamp));
+              
+                subscribers[subscriberId].video.prevBytesReceived = videoStats.bytesReceived;
+                subscribers[subscriberId].video.prevPacketsReceived = videoStats.packetsReceived;
+                subscribers[subscriberId].video.prevPacketsLost = videoStats.packetsLost;
+                subscribers[subscriberId].video.previousTimestamp = statsArray.timestamp;
+                subscribers[subscriberId].audio.prevBytesReceived = audioStats.bytesReceived;
+                subscribers[subscriberId].audio.prevPacketsReceived = audioStats.packetsReceived;
+                subscribers[subscriberId].audio.prevPacketsLost = audioStats.packetsLost;
+                subscribers[subscriberId].audio.previousTimestamp = statsArray.timestamp;
                 
                 if(onSubscriberStatsAvailableListener != null){
-                    onSubscriberStatsAvailableListener(sub.id,videoKbps,videoStats.frameRate,PLRatio);
+                    onSubscriberStatsAvailableListener(sub.id,{
+                            bandwidth: videoKbps,
+                            width: sub.videoWidth(),
+                            height: sub.videoHeight(),
+                            frameRate: Math.round(videoStats.frameRate),
+                            packetLoss: videoPLRatio
+                        },
+                        {
+                            bandwidth: audioKbps,
+                            packetLoss: audioPLRatio
+                        }
+                    );
+                                                       
                 }
                 else {
                     console.log("onStatsAvailableListener is null");
@@ -116,8 +166,10 @@ OTStats = (options) => {
   
   return {
       addSubscriber,
+      removeSubscriber,
       setPublisher,
       start,
+      stop,
       setPublisherOnStatsAvailableListener,
       setSubscriberOnStatsAvailableListener
   };
