8b0af8c0df69cc1c938155d16d4ad4ea905408d8
rktalusani <rajkiran.talusani@vonage.com>
2021-10-29 11:08:04 +0800

    add chrome fix and remove nonegains

--- public/lib/spacial-audio/spacial-audio.js
+++ public/lib/spacial-audio/spacial-audio.js
@@ -3,11 +3,11 @@ OTSpacialAudio = () => {
 let isSupported = false;
 let audioContext = undefined;
 let resonanceAudioScene = undefined;
+let mediaDestinationNode = undefined;
 let resonanceGain = undefined;
 var resonanceSources = {};
-var noneGains = {};
-
-let subscriberStreamMap = {};
+var audioEl = undefined;
+let subscriberMap = {};
 let MODE_NONE=0;
 let MODE_SPATIAL=2;
 
@@ -35,6 +35,9 @@ function setAudioMode(mode){
 }
 
 function initResonanceAudio() {
+    audioEl = new Audio();
+    audioEl.setAttribute("autoplay", "autoplay");
+    audioEl.setAttribute("playsinline", "playsinline");
     try {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     audioContext = new window.AudioContext();
@@ -44,13 +47,17 @@ function initResonanceAudio() {
     return;
   }
    isSupported = true;
+    mediaDestinationNode = audioContext.createMediaStreamDestination();
     resonanceGain = audioContext.createGain();
     resonanceAudioScene = new ResonanceAudio(audioContext,{
         ambisonicOrder: 1
     });
     
     resonanceAudioScene.output.connect(resonanceGain);
-    resonanceGain.connect(audioContext.destination);
+    if(/chrome/i.test(navigator.userAgent))
+        fixChrome687574(mediaDestinationNode,audioContext,resonanceGain,audioEl);
+    else
+        resonanceGain.connect(audioContext.destination);
 
     let roomDimensions = {
         width: roomWidth,
@@ -78,20 +85,20 @@ function changeMode(mode){
     if(mode == MODE_SPATIAL){
         console.log("mode is spatial now");
         resonanceGain.gain.value=1;
-        setNoneGainValue(0);
+        setSubscribersVolume(0);
     }
     else if(mode == MODE_NONE){
         console.log("mode is mono now");
         resonanceGain.gain.value=0;
-        setNoneGainValue(1);
+        setSubscribersVolume(50);
     }
 }
 
-function setNoneGainValue(val){
+function setSubscribersVolume(vol){
     if(!isSupported)
         return;
-    for (var streamId in noneGains) {
-        noneGains[streamId].gain.value=val;
+    for (var streamId in subscriberMap) {
+        subscriberMap[streamId].setAudioVolume(vol);
     }
 }
 
@@ -99,8 +106,8 @@ function disconnectVideoFromResonance(subscriber){
     let streamId = subscriber.id;
     if(resonanceSources[streamId])
         resonanceSources[streamId].disconnect();
-    if(noneGains[streamId])
-        noneGains[streamId].disconnect();
+    if(subscriberMap[streamId])
+        delete subscriberMap[streamId];
 }
 
 function adjustAudioSourcePositions(streams, numSpeakersVisible, layoutDiv){
@@ -131,38 +138,24 @@ function connectVideoToResonanceAudio(subscriber,x=1,y=0,z=1) {
     if(!isSupported)
         return;
     let subscriberId = subscriber.id;
-    subscriber.setAudioVolume(1);
+    subscriberMap[subscriberId] = subscriber;
+    if(audioMode == MODE_SPATIAL)
+        subscriber.setAudioVolume(0);
     
     console.log("Adding streamId="+subscriber.stream.id+" to the map");
     /* find the video element */
-    var videoElem = undefined;
-    var subscriberElem = document.getElementById(subscriberId);
-    var children = subscriberElem.getElementsByTagName('*');
-    for (var i = -1, l = children.length; ++i < l;) {
-        if(children[i].nodeName=="VIDEO"){
-            videoElem = children[i];
-        }
-    }
+    var videoElem = subscriber.element.querySelector('video');
     if(videoElem == undefined){
         console.log("Video Element null in connectVideoToResonanceAudio. Something terribly wrong");
         return;
     }
     
-    let audioElementSource = audioContext.createMediaStreamSource(videoElem.captureStream ? videoElem.captureStream():videoElem.mozCaptureStream());
+    //console.log(videoElem.srcObject);
+    let audioElementSource = audioContext.createMediaStreamSource(/*videoElem.captureStream ? videoElem.captureStream():videoElem.mozCaptureStream()*/videoElem.srcObject);
     let source = resonanceAudioScene.createSource();
     audioElementSource.connect(source.input);
     source.setPosition(x, y, z);
     resonanceSources[subscriberId] = source;
-    
-    let noneGain = audioContext.createGain();
-    audioElementSource.connect(noneGain);
-    noneGain.connect(audioContext.destination);
-    noneGains[subscriberId] = noneGain;
-    if(audioMode == MODE_SPATIAL)
-        noneGain.gain.value=0;
-    else
-        noneGain.gain.value=1;
-    
 }
 
 function setSourcePosition(streamId,x,y,z){
@@ -188,6 +181,54 @@ function setListenerPosition(x,y,z){
         return;
 	resonanceAudioScene.setListenerPosition(x,y,z);
     audioContext.listener.setPosition(x, y, z);
+}
+    
+function fixChrome687574(loopbackDestination, audioContext, resonanceGainNode,audioEl){
+  const outboundPeerConnection = new RTCPeerConnection();
+  const inboundPeerConnection = new RTCPeerConnection();
+
+  const onError = e => {
+    console.error("RTCPeerConnection loopback initialization error", e);
+  };
+
+  outboundPeerConnection.addEventListener("icecandidate", e => {
+    inboundPeerConnection.addIceCandidate(e.candidate).catch(onError);
+  });
+
+  inboundPeerConnection.addEventListener("icecandidate", e => {
+    outboundPeerConnection.addIceCandidate(e.candidate).catch(onError);
+  });
+
+  inboundPeerConnection.addEventListener("track", e => {
+    //var tempSource = audioContext.createMediaStreamSource(e.streams[0]);
+    //tempSource.connect(audioContext.destination);
+      audioEl.srcObject = e.streams[0];
+  });
+
+  resonanceGainNode.connect(loopbackDestination);
+
+  loopbackDestination.stream.getTracks().forEach(track => {
+    outboundPeerConnection.addTrack(track, loopbackDestination.stream);
+  });
+
+  outboundPeerConnection.createOffer().then(offer => {
+    outboundPeerConnection.setLocalDescription(offer).catch(onError);
+
+    inboundPeerConnection
+      .setRemoteDescription(offer)
+      .then(() => {
+        inboundPeerConnection
+          .createAnswer()
+          .then(answer => {
+            answer.sdp = answer.sdp.replace('useinbandfec=1', 'useinbandfec=1; stereo=1');
+            inboundPeerConnection.setLocalDescription(answer).catch(onError);
+
+            outboundPeerConnection.setRemoteDescription(answer).catch(onError);
+          })
+          .catch(onError);
+      })
+      .catch(onError);
+  });
 }
 
     return {
